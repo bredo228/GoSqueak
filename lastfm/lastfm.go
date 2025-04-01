522c82fe8165218ecf32b0f82f6bf1ff1fcd0044
bredo <bredo@bredo.tech>
2025-04-02 12:34:26 +1300

    feat(lastfm): start work on lastfm album art

---
+++ lastfm/lastfm.go
@@ -0,0 +1,13 @@
+package lastfm
+
+import (
+	. "github.com/bredo228/GoSqueak/datatypes"
+)
+
+func GetTrackArtwork(track Track, config Config) string {
+	return "some album art"
+}
+
+func DoLastFmRequest() string {
+	return "hi"
+}
