998372a8460ada3e7a8272846644a50c1692ac0d
bredo <bredo@bredo.tech>
2025-09-07 17:10:51 +1200

    feat(config): add config loader

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -1,5 +1,10 @@
 package config
 
+import (
+	"encoding/json"
+	"os"
+)
+
 type Config struct {
 	OscPort             int    `json:"oscPort"`
 	WebsocketListenPort int    `json:"websocketListenPort"`
@@ -7,4 +12,16 @@ type Config struct {
 	LastFmEnabled       bool   `json:"lastFmEnabled"`
 	LastFmUsername      string `json:"lastFmUsername"`
 	LastFmApiKey        string `json:"lastFmApiKey"`
+}
+
+func LoadConfig(filename string, config *Config) error {
+
+	c, err := os.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(c, &config)
+	return err
+
 }
