b530b8fa96e6fd726990327bec4c4dc974e77e19
bredo <bredo@bredo.tech>
2025-09-07 17:15:52 +1200

    feat(config): load config

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,18 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/bredo228/GoSqueak/cmd/config"
+)
+
+var (
+	conf config.Config
+)
 
 func main() {
 	log.Println("Starting GoSqueak v2")
+
+	config.LoadConfig("./config.json", &conf)
 
 }
